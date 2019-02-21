949af5fab9b9eaf49d5247b67d5c07b69729fc7c
unknown <cyhoyvanov@gmail.com>
2019-02-21 13:39:19 +0200

    first commit

---
+++ src/Homework2.java
@@ -0,0 +1,11 @@
+
+public class Homework2 {
+	 public static void main(String[] args) {
+	      double n = 32d;
+	      double first = n / 10;
+	      double firstS = Math.floor(first);
+	      double second = n % 10;
+	      double sum = firstS + second;
+	      System.out.println("Result=" + sum);
+	    }
+	}
